11d0173f79a47db1ee0bb2667c13ff8647fc143a
Nadeem <nanvari@live.com>
2020-01-23 16:54:15 -0600

    add initial skeleton for backend plus coaches table for database

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+module.exports = server;
