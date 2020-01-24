ac3e28cc30d79c35936b8b19582f8825aece52d7
macjabeth <macbethjonathan@gmail.com>
2020-01-24 11:38:31 -0500

    add comments

--- api/server.js
+++ api/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+// create server
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+// first endpoint
 server.get('/', (req, res) => {
     res.status(200).json({ server: "server is up"})
 })
