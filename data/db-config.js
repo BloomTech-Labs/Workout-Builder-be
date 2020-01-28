a57a550fbd11d14b3c3a50e0b2753fccb5215e6b
Nadeem <nanvari@live.com>
2020-01-28 17:35:54 -0600

    install postgres dependency, update knexfile.js and db-config.js file for production

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const knex = require('knex');
-
 const config = require('../knexfile.js');
+const environment = process.env.DB_CONNECT || 'development';
 
-module.exports = knex(config.development);
+module.exports = knex(config[environment]);
