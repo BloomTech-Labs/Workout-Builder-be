50698912cf9c8ef929ae063c6c7a65a61a16d547
macjabeth <macbethjonathan@gmail.com>
2020-02-10 12:36:18 -0500

    add sentry configuration to server

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const authRouter = require('../auth/authRouter'); 
+const authRouter = require('../auth/authRouter');
 
 // create server
 const server = express();
+const Sentry = require('@sentry/node');
 
+Sentry.init({ dsn: process.env.SENTRY_DSN });
+
+// The request handler must be the first middleware on the app
+server.use(Sentry.Handlers.requestHandler());
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
@@ -16,5 +21,8 @@ server.get('/', (req, res) => {
 });
 
 server.use('/auth',authRouter);
+
+// The error handler must be before any other error middleware and after all controllers
+server.use(Sentry.Handlers.errorHandler());
 
 module.exports = server;
