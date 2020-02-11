e65fa75f3830515e2874ff6a32bd6560d77bed15
Nadeem <nanvari@live.com>
2020-02-11 15:35:04 -0600

    build exercisesArray to insert into exercises_workouts table
    Co-authored-by: Burl <burlferns@gmail.com>

--- programs/programsRouter.js
+++ programs/programsRouter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-multiple-empty-lines */
 const express = require('express');
 const router = express.Router();
 const Programs = require('./programs-model');
@@ -26,6 +27,27 @@ router.post('/', validTokenCheck, (req, res) => {
       console.log(workoutsArray);
       Workouts.addWorkout(workoutsArray)
         .then(savedWorkouts => {
+          // workoutIdArray will return an array of the workout IDs for the program
+          let workoutIdArray = savedWorkouts.map(el => el.id);
+          console.log(workoutIdArray, '<-- workoutIdArray');
+
+          let exercisesArray = [];
+          req.body.workouts.forEach((elW, indexW) => {
+            elW.exercises.forEach((elE, indexE) => {
+              let tempObject2 = {};
+              tempObject2.exercise_id = elE.id;
+              tempObject2.exercise_details = elE.exercise_details;
+              tempObject2.order = indexE + 1;
+              tempObject2.workout_id = workoutIdArray[indexW];
+              exercisesArray.push(tempObject2);
+            });
+          });
+
+          console.log(exercisesArray, '<-- exercisesArray');
+
+
+
+
           res.status(201).json(savedWorkouts);
         });
     })
