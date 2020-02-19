f9cd3e4271c4455415ef24b9ac35bd533cba3f3e
Nadeem <nanvari@live.com>
2020-02-19 13:49:33 -0600

    add comments to clients-model, add chained thens in delete endpoint

--- clients/clientsRouter.js
+++ clients/clientsRouter.js
@@ -82,14 +82,16 @@ router.delete('/:id', validTokenCheck, (req, res) => {
   Clients.getClientById(id)
     .then(client => {
       if (client && client.coach_id === coach_id) {
-        Clients.deleteClient(id)
-          .then(client => {
-            res.status(200).json(client);
-          });
+        return Clients.deleteClient(id);
       } else if (!client) {
         res.status(404).json({ error: `cannot delete client with id: ${id} because it does not exist` });
       } else {
         res.status(403).json({ error: `you are not authorized to delete client with id: ${id}` });
+      }
+    })
+    .then(client => {
+      if (client) {
+        res.status(200).json(client);
       }
     })
     .catch(error => {
